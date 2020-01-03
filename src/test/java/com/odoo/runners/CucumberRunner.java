56b72f4b3ee39b0ddacf988a265170ad29e9fae0
Sahira M <55321053+Sahiras@users.noreply.github.com>
2020-01-03 17:36:11 -0600

    Update CucumberRunner.java

--- src/test/java/com/odoo/runners/CucumberRunner.java
+++ src/test/java/com/odoo/runners/CucumberRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "com/odoo/stepDefinitions",
+        glue = "com/odoo/step_definitions",
         dryRun = false,
         tags = "@CreateOpportunity",
     
