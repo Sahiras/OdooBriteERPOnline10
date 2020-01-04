9ed2baa4fc35438a970ca80d172ad726529bca86
Sahira M <55321053+Sahiras@users.noreply.github.com>
2020-01-03 18:03:12 -0600

    Update LoginPage.java

--- src/test/java/com/odoo/pages/LoginPage.java
+++ src/test/java/com/odoo/pages/LoginPage.java
@@ -1,9 +1,15 @@
 package com.odoo.pages;
 
 import com.odoo.utilities.ConfigurationReader;
+import org.openqa.selenium.support.PageFactory;
+import com.odoo.utilities.ConfigurationReader;
+import com.odoo.utilities.Driver;
 
 
-public class LoginPage {
+public class LoginPage extends BasePage {
+    public LoginPage(){
+
+        PageFactory.initElements(Driver.get(),this);
 
     public void login(String username, String password) {
         login(ConfigurationReader.getProperty("username"), ConfigurationReader.getProperty("password"));
