3b412d8350baf37f551061bb539299e09e5c9697
Sahira M <55321053+Sahiras@users.noreply.github.com>
2020-01-07 17:48:28 -0600

    Update CucumberRunner.java

--- src/test/java/com/odoo/runners/CucumberRunner.java
+++ src/test/java/com/odoo/runners/CucumberRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/odoo/step_definitions",
         dryRun = false,
-        tags = "@POS_Activities"
+        tags = ""
    
         plugin = {"html:target/default-cucumber-reports",
         "json:target/cucumber.json",
