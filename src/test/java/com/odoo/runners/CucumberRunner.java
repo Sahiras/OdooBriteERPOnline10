da17373b583e2a28847c5193b264f485dae61854
ofmagden <arbesta41@gmail.com>
2020-01-07 18:13:03 -0500

    PointOfSale " Page, Step def,features " are created.

--- src/test/java/com/odoo/runners/CucumberRunner.java
+++ src/test/java/com/odoo/runners/CucumberRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/odoo/step_definitions",
         dryRun = false,
-        tags = "@CreateOpportunity"
+        tags = "@POS_Activities"
     
 
 )
