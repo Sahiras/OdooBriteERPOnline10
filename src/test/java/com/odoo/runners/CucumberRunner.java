edb1eca8e8301183c4340cf734dbf9fbf7dc8b03
esmayorulmaz <esmayorulmaz2@gmail.com>
2020-01-09 15:22:26 -0600

    final version of ContactPge, Contact.feature and Stepdefinitions

--- src/test/java/com/odoo/runners/CucumberRunner.java
+++ src/test/java/com/odoo/runners/CucumberRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com/odoo/step_definitions",
-        dryRun = false,
+        dryRun =false,
         tags ="@Contact",
         plugin = {"html:target/default-cucumber-reports",
         "json:target/cucumber.json",
