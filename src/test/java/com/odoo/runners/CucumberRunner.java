c430806367fe0cf80f8b67abcef84d857dc576f7
Sahira M <55321053+Sahiras@users.noreply.github.com>
2020-01-17 07:21:00 -0600

    Update CucumberRunner.java

--- src/test/java/com/odoo/runners/CucumberRunner.java
+++ src/test/java/com/odoo/runners/CucumberRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         glue = "com/odoo/step_definitions",
 
         dryRun = false,
-        tags = "@CreateOppurtunity_SelectAPerson",
+      //  tags = "@CreateOppurtunity_SelectAPerson",
         plugin = {"html:target/default-cucumber-reports",
         "json:target/cucumber.json",
         "rerun:target/rerun.txt"}
